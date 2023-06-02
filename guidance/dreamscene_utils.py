8063161ec4ee2b40f3555353cdd3176306a0d1c3
Zhijie Lin <136380243@qq.com>
2023-06-03 01:29:36 +0800

    update

--- guidance/dreamscene_utils.py
+++ guidance/dreamscene_utils.py
@@ -212,8 +212,8 @@ class DreamScene(nn.Module):
             model_uncond_sd, model_t_sd = model_output_sd.chunk(2)
             model_output_sd = model_uncond + guidance_scale * (model_t_sd - model_uncond_sd)
             if self.model.parameterization == "v":
-                e_t = self.model.predict_eps_from_z_and_v(latents, t, model_output)
-                e_t_sd = self.model.predict_eps_from_z_and_v(latents, t, model_output_sd)
+                e_t = self.model.predict_eps_from_z_and_v(latents_noisy, t, model_output)
+                e_t_sd = self.model.predict_eps_from_z_and_v(latents_noisy, t, model_output_sd)
             else:
                 e_t = model_output
                 e_t_sd = model_output_sd
