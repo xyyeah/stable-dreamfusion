debefa2dd7a1d18f055a916c8c8e6a075e159322
Zhijie Lin <136380243@qq.com>
2023-06-03 21:47:46 +0800

    update

--- ds_tests/train_teddy_p3.sh
+++ ds_tests/train_teddy_p3.sh
@@ -15,9 +15,10 @@
          --seed 1
          # --default_radius 2.0 --radius_range 1.6 2.4
 
-   python3.9 main.py -O --image ./data/teddy_rgba.png \
+   python3.9 main.py -O --image ./data/teddy_rgba.png  --dreamscene\
    --text "a brown teddy bear sitting on a ground" --negative "low quality" \
-   --workspace /workspace/teddy_vsd --test_interval 2 --save_guidance_interval 50
+   --workspace /workspace/teddy_vsd --test_interval 2 --save_guidance_interval 50 \
+   --lambda_2d_normal_smooth 0.0 --lambda_depth 0.0 --lambda_rgb 0.0  --lambda_mask 0.0
 
 # # # Phase 1
 # python3.9 main.py -O --image /mnt/cache_sail/liulj/stable-dreamfusion/data/teddy_rgba.png \
