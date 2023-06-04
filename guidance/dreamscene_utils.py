e3c8ef3d56351089a1e55101adb9593af0870fd0
Zhijie Lin <136380243@qq.com>
2023-06-04 16:55:42 +0800

    update

--- guidance/dreamscene_utils.py
+++ guidance/dreamscene_utils.py
@@ -222,7 +222,7 @@ class DreamScene(nn.Module):
             # model_t_sd, render_rgb = self.model.apply_model(latents_noisy_768, torch.cat([t]).to(self.device), cond, return_rgb=True)
             # model_uncond_sd = self.model.apply_model(latents_noisy_768, torch.cat([t]).to(self.device), uncond, return_rgb=False)
             # render_rgb = render_rgb.chunk(2)[1]
-            model_output_sd = model_uncond_sd + guidance_scale * (model_t_sd - model_uncond_sd)
+            model_output_sd = model_uncond_sd + 3.0 * (model_t_sd - model_uncond_sd)
             e_t_sd = self.model.predict_eps_from_z_and_v(latents_noisy_768, t, model_output_sd)
             # e_t_sd2 = self.model.predict_noise_from_start(latents_noisy_768, t, render_rgb)
 
